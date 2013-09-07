444ed445670cd1b7db9442cbf0d02bd29246bf29
cathames <repos@cathames.com>
2013-09-07 11:26:37 -0700

    instantiated by: "rails new ror-giant"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorGiant::Application.config.secret_key_base = 'bb7589d754388251f1c73a982e197cf6b46a03caa57e3bb3c9b322835918af2f5217f1bab92671242b48294d6fb369e69a9912cc864e29bef49c8f60ceb26c96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RorGiant::Application.config.session_store :cookie_store, key: '_ror-giant_session'
