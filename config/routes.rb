d4bad3d450ea76da612164e06d9bc54fcf93cbd1
christopheraugustus <repos@christopheraugustus.com>
2014-09-08 20:42:21 -0700

    routes modified for job applications and referrals (tell a friend)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 RorGiant::Application.routes.draw do
   root 'welcome#index'
   resources :jobs, only: [:index, :show] do
-    member do
-      get 'apply'
-      get 'tell'
-    end
+    resources :job_applications,  only: :new,     to: 'jobs#application'
+    resources :job_applications,  only: :create,  to: 'jobs#apply'
+    resources :job_referrals,     only: :new,     to: 'jobs#referral'
+    resources :job_referrals,     only: :create,  to: 'jobs#refer'
   end
 end
