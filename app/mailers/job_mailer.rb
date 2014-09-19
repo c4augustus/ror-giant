09ee44ab1ae0ab836c220b088be26577d6a48a28
christopheraugustus <repos@christopheraugustus.com>
2014-09-19 13:55:46 -0700

    implemented JobMailer

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -1,3 +1,31 @@
 class JobMailer < ActionMailer::Base
-  default from: "from@example.com"
+  #default from: "from@example.com"
+
+  def email_address_for_resumes
+    "resumes@#{default_url_options[:host]}"
+  end
+
+  def mail_application(job_application)
+    @job_application = job_application
+    @job = @job_application.job
+    puts "## @job_application==#{job_application}"
+    @subject = "Application for #{reference_to_job}"
+    mail from: @job_application.email,
+           to: email_address_for_resumes,
+      subject: @subject
+  end
+
+  def mail_referral(job_referral)
+    @job_referral = job_referral
+    @job = @job_referral.job
+    @subject = "??? wants to you know about #{reference_to_job}"
+    mail from: email_address_for_resumes,
+           to: @job_referral.email,
+      subject: @subject
+  end
+
+private
+  def reference_to_job
+     "job ##{@job.id_scheme_ext}: #{@job.title}"
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,10 @@ module RorGiant
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.action_mailer.default_url_options = {
+      host: 'giantrecruiting.com'
+    }
+
     def jobs_facility
       @jobs_facility ||= JobsFacility.new
     end
