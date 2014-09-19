c632e910340c50ff80ae6c56d486609269462796
christopheraugustus <repos@christopheraugustus.com>
2014-09-19 13:52:20 -0700

    added Job#apply(...) and #refer(...) to encapsulate creation of applications and referrals

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,21 +2,30 @@ class Job
   include Mongoid::Document
   has_many :job_application
   has_many :job_referral
-  field :id_scheme_ext, type: String
-  field :title, type: String
-  field :location, type: String
+  field :id_scheme_ext,   type: String
+  field :title,           type: String
+  field :location,        type: String
   field :employment_type, type: String
-  field :start_date, type: Date
-  field :pay_rate, type: String
-  field :salary, type: String
-  field :salary_unit, type: String
-  field :description, type: String
+  field :start_date,      type: Date
+  field :pay_rate,        type: String
+  field :salary,          type: String
+  field :salary_unit,     type: String
+  field :description,     type: String
 
   def self.categories
-    @@categories ||= ["UXD", "CREATIVE", "MARKETING"]
+    @@categories ||= ['UXD', 'CREATIVE', 'MARKETING']
   end
 
   def category
     "CREATIVE"
   end
+
+  def apply(attributes)
+    JobApplication.create(attributes.merge!({job: self}))
+  end
+
+  def refer(attributes)
+    JobReferral.create(attributes.merge!({job: self}))
+  end
+
 end
