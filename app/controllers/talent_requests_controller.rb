c869af85432ab07fab0c0a01a8fcbc7c468aed77
christopheraugustus <repos@christopheraugustus.com>
2014-12-16 00:51:05 -0800

    added possible future work, commented out

--- app/controllers/talent_requests_controller.rb
+++ app/controllers/talent_requests_controller.rb
@@ -4,6 +4,19 @@ class TalentRequestsController < ApplicationController
   end
 
   def create
+=begin @@@ FUTURE WORK
+    @talent_request = TalentRequest.new(params.require(:talent_request).permit!))
+    respond_to do |format|
+      if @job.save
+        format.html { redirect_to @job, notice: 'Job was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @job }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+    respond_with
+=end
     if (talent_request = TalentRequest.create(params.require(:talent_request).permit!))
       AppMailer.mail_talent_request(talent_request).deliver
     end
